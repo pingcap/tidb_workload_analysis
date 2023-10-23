e86bb9184e713380b16e5d3db7fce45c4308dbc6
qw4990 <zhangyuanjia@pingcap.com>
2023-10-23 11:15:46 +0800

    fixup

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"github.com/pingcap/index_advisor/cmd"
+	"github.com/pingcap/index_advisor/version"
 	"github.com/spf13/cobra"
 )
 
 var (
 	rootCmd = &cobra.Command{
-		Use:   "TiDB-index-advisor",
-		Short: "TiDB index advisor",
-		Long:  `TiDB index advisor recommends you the best indexes for your workload`,
+		Use:     "TiDB-index-advisor",
+		Short:   "TiDB index advisor",
+		Long:    `TiDB index advisor recommends you the best indexes for your workload`,
+		Version: version.Version(),
 	}
 )
 
