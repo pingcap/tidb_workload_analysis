ca7a21247d83f3cbc88b5e979d6b18c9ba185692
qw4990 <zhangyuanjia@pingcap.com>
2024-03-12 16:40:05 +0800

    update ossinsight result

--- examples/ossinsight/output/ddl.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX idx_type_action_actor_id ON gharchive_dev.github_events (type, action, actor_id);
-CREATE INDEX idx_type_action_created_at ON gharchive_dev.github_events (type, action, created_at);
-CREATE INDEX idx_type_repo_id_action ON gharchive_dev.github_events (type, repo_id, action);
-CREATE INDEX idx_type_repo_id_repo_name ON gharchive_dev.github_events (type, repo_id, repo_name);
-CREATE INDEX idx_stars_primary_language_pushed_at ON gharchive_dev.github_repos (stars, primary_language, pushed_at)
