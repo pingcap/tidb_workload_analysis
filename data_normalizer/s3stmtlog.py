b877a93c18c76c107fcef5071635f7e5b80e9653
qw4990 <zhangyuanjia@pingcap.com>
2024-04-17 15:15:11 +0800

    add more tests for normalizer

--- data_normalizer/s3stmtlog.py
+++ data_normalizer/s3stmtlog.py
@@ -1,111 +1,6 @@
+from schema import statement_summary_schema
 import json
-
-table_schema = """
-CREATE TABLE IF NOT EXISTS `STATEMENTS_SUMMARY` (
-  `SUMMARY_BEGIN_TIME` timestamp DEFAULT NULL,
-  `SUMMARY_END_TIME` timestamp DEFAULT NULL,
-  `STMT_TYPE` varchar(64) DEFAULT NULL,
-  `SCHEMA_NAME` varchar(64) DEFAULT NULL,
-  `DIGEST` varchar(64) DEFAULT NULL,
-  `DIGEST_TEXT` text DEFAULT NULL,
-  `TABLE_NAMES` text DEFAULT NULL,
-  `INDEX_NAMES` text DEFAULT NULL,
-  `SAMPLE_USER` varchar(64) DEFAULT NULL,
-  `EXEC_COUNT` bigint(20) DEFAULT NULL,
-  `SUM_ERRORS` int(11) DEFAULT NULL,
-  `SUM_WARNINGS` int(11) DEFAULT NULL,
-  `SUM_LATENCY` bigint(20) DEFAULT NULL,
-  `MAX_LATENCY` bigint(20) DEFAULT NULL,
-  `MIN_LATENCY` bigint(20) DEFAULT NULL,
-  `AVG_LATENCY` bigint(20) DEFAULT NULL,
-  `AVG_PARSE_LATENCY` bigint(20) DEFAULT NULL,
-  `MAX_PARSE_LATENCY` bigint(20) DEFAULT NULL,
-  `AVG_COMPILE_LATENCY` bigint(20) DEFAULT NULL,
-  `MAX_COMPILE_LATENCY` bigint(20) DEFAULT NULL,
-  `SUM_COP_TASK_NUM` bigint(20) DEFAULT NULL,
-  `MAX_COP_PROCESS_TIME` bigint(20) DEFAULT NULL,
-  `MAX_COP_PROCESS_ADDRESS` varchar(256) DEFAULT NULL,
-  `MAX_COP_WAIT_TIME` bigint(20) DEFAULT NULL,
-  `MAX_COP_WAIT_ADDRESS` varchar(256) DEFAULT NULL,
-  `AVG_PROCESS_TIME` bigint(20) DEFAULT NULL,
-  `MAX_PROCESS_TIME` bigint(20) DEFAULT NULL,
-  `AVG_WAIT_TIME` bigint(20) DEFAULT NULL,
-  `MAX_WAIT_TIME` bigint(20) DEFAULT NULL,
-  `AVG_BACKOFF_TIME` bigint(20) DEFAULT NULL,
-  `MAX_BACKOFF_TIME` bigint(20) DEFAULT NULL,
-  `AVG_TOTAL_KEYS` bigint(20) DEFAULT NULL,
-  `MAX_TOTAL_KEYS` bigint(20) DEFAULT NULL,
-  `AVG_PROCESSED_KEYS` bigint(20) DEFAULT NULL,
-  `MAX_PROCESSED_KEYS` bigint(20) DEFAULT NULL,
-  `AVG_ROCKSDB_DELETE_SKIPPED_COUNT` double DEFAULT NULL,
-  `MAX_ROCKSDB_DELETE_SKIPPED_COUNT` int(11) DEFAULT NULL,
-  `AVG_ROCKSDB_KEY_SKIPPED_COUNT` double DEFAULT NULL,
-  `MAX_ROCKSDB_KEY_SKIPPED_COUNT` int(11) DEFAULT NULL,
-  `AVG_ROCKSDB_BLOCK_CACHE_HIT_COUNT` double DEFAULT NULL,
-  `MAX_ROCKSDB_BLOCK_CACHE_HIT_COUNT` int(11) DEFAULT NULL,
-  `AVG_ROCKSDB_BLOCK_READ_COUNT` double DEFAULT NULL,
-  `MAX_ROCKSDB_BLOCK_READ_COUNT` int(11) DEFAULT NULL,
-  `AVG_ROCKSDB_BLOCK_READ_BYTE` double DEFAULT NULL,
-  `MAX_ROCKSDB_BLOCK_READ_BYTE` int(11) DEFAULT NULL,
-  `AVG_PREWRITE_TIME` bigint(20) DEFAULT NULL,
-  `MAX_PREWRITE_TIME` bigint(20) DEFAULT NULL,
-  `AVG_COMMIT_TIME` bigint(20) DEFAULT NULL,
-  `MAX_COMMIT_TIME` bigint(20) DEFAULT NULL,
-  `AVG_GET_COMMIT_TS_TIME` bigint(20) DEFAULT NULL,
-  `MAX_GET_COMMIT_TS_TIME` bigint(20) DEFAULT NULL,
-  `AVG_COMMIT_BACKOFF_TIME` bigint(20) DEFAULT NULL,
-  `MAX_COMMIT_BACKOFF_TIME` bigint(20) DEFAULT NULL,
-  `AVG_RESOLVE_LOCK_TIME` bigint(20) DEFAULT NULL,
-  `MAX_RESOLVE_LOCK_TIME` bigint(20) DEFAULT NULL,
-  `AVG_LOCAL_LATCH_WAIT_TIME` bigint(20) DEFAULT NULL,
-  `MAX_LOCAL_LATCH_WAIT_TIME` bigint(20) DEFAULT NULL,
-  `AVG_WRITE_KEYS` double DEFAULT NULL,
-  `MAX_WRITE_KEYS` bigint(20) DEFAULT NULL,
-  `AVG_WRITE_SIZE` double DEFAULT NULL,
-  `MAX_WRITE_SIZE` bigint(20) DEFAULT NULL,
-  `AVG_PREWRITE_REGIONS` double DEFAULT NULL,
-  `MAX_PREWRITE_REGIONS` int(11) DEFAULT NULL,
-  `AVG_TXN_RETRY` double DEFAULT NULL,
-  `MAX_TXN_RETRY` int(11) DEFAULT NULL,
-  `SUM_EXEC_RETRY` bigint(20) DEFAULT NULL,
-  `SUM_EXEC_RETRY_TIME` bigint(20) DEFAULT NULL,
-  `SUM_BACKOFF_TIMES` bigint(20) DEFAULT NULL,
-  `BACKOFF_TYPES` varchar(1024) DEFAULT NULL,
-  `AVG_MEM` bigint(20) DEFAULT NULL,
-  `MAX_MEM` bigint(20) DEFAULT NULL,
-  `AVG_DISK` bigint(20) DEFAULT NULL,
-  `MAX_DISK` bigint(20) DEFAULT NULL,
-  `AVG_KV_TIME` bigint(22) DEFAULT NULL,
-  `AVG_PD_TIME` bigint(22) DEFAULT NULL,
-  `AVG_BACKOFF_TOTAL_TIME` bigint(22) DEFAULT NULL,
-  `AVG_WRITE_SQL_RESP_TIME` bigint(22) DEFAULT NULL,
-  `MAX_RESULT_ROWS` bigint(22) DEFAULT NULL,
-  `MIN_RESULT_ROWS` bigint(22) DEFAULT NULL,
-  `AVG_RESULT_ROWS` bigint(22) DEFAULT NULL,
-  `PREPARED` tinyint(1) DEFAULT NULL,
-  `AVG_AFFECTED_ROWS` double DEFAULT NULL,
-  `FIRST_SEEN` timestamp DEFAULT NULL,
-  `LAST_SEEN` timestamp DEFAULT NULL,
-  `PLAN_IN_CACHE` tinyint(1) DEFAULT NULL,
-  `PLAN_CACHE_HITS` bigint(20) DEFAULT NULL,
-  `PLAN_IN_BINDING` tinyint(1) DEFAULT NULL,
-  `QUERY_SAMPLE_TEXT` text DEFAULT NULL,
-  `PREV_SAMPLE_TEXT` text DEFAULT NULL,
-  `PLAN_DIGEST` varchar(64) DEFAULT NULL,
-  `PLAN` text DEFAULT NULL,
-  `BINARY_PLAN` text DEFAULT NULL,
-  `CHARSET` varchar(64) DEFAULT NULL,
-  `COLLATION` varchar(64) DEFAULT NULL,
-  `PLAN_HINT` varchar(64) DEFAULT NULL,
-  `MAX_REQUEST_UNIT_READ` double DEFAULT NULL,
-  `AVG_REQUEST_UNIT_READ` double DEFAULT NULL,
-  `MAX_REQUEST_UNIT_WRITE` double DEFAULT NULL,
-  `AVG_REQUEST_UNIT_WRITE` double DEFAULT NULL,
-  `MAX_QUEUED_RC_TIME` bigint(22) DEFAULT NULL,
-  `AVG_QUEUED_RC_TIME` bigint(22) DEFAULT NULL,
-  `RESOURCE_GROUP` varchar(64) DEFAULT NULL
-);
-"""
+import os
 
 def single_quote(s):
     return "'" + s.replace("'", "''").replace("\\''", "\\'") + "'"
@@ -137,73 +32,25 @@ def s3stmt2sql(one_stmt_json):
         "AVG_LATENCY": d['sum_latency'] / d['exec_count'],
         "AVG_PARSE_LATENCY": d['sum_parse_latency'] / d['exec_count'],
         "AVG_COMPILE_LATENCY": d['sum_compile_latency'] / d['exec_count'],
-        "SUM_COP_TASK_NUM": d['sum_num_cop_tasks'],
-        "AVG_PROCESS_TIME": d['sum_process_time'] / d['exec_count'],
-        "AVG_WAIT_TIME": d['sum_wait_time'] / d['exec_count'],
-        "AVG_BACKOFF_TIME": d['sum_wait_time'] / d['exec_count'],
-        "AVG_TOTAL_KEYS": d['sum_total_keys'] / d['exec_count'],
-        "AVG_PROCESSED_KEYS": d['sum_processed_keys'] / d['exec_count'],
-        "AVG_ROCKSDB_DELETE_SKIPPED_COUNT": d['sum_rocksdb_delete_skipped_count'] / d['exec_count'],
-        "AVG_ROCKSDB_KEY_SKIPPED_COUNT": d['sum_rocksdb_key_skipped_count'] / d['exec_count'],
-        "AVG_ROCKSDB_BLOCK_CACHE_HIT_COUNT": d['sum_rocksdb_block_cache_hit_count'] / d['exec_count'],
-        "AVG_ROCKSDB_BLOCK_READ_COUNT": d['sum_rocksdb_block_read_count'] / d['exec_count'],
-        "AVG_ROCKSDB_BLOCK_READ_BYTE": d['sum_rocksdb_block_read_byte'] / d['exec_count'],
-        "AVG_PREWRITE_TIME": d['sum_prewrite_time'] / d['exec_count'],
-        "AVG_COMMIT_TIME": d['sum_commit_time'] / d['exec_count'],
-        "AVG_GET_COMMIT_TS_TIME": d['sum_get_commit_ts_time'] / d['exec_count'],
-        "AVG_COMMIT_BACKOFF_TIME": d['sum_commit_backoff_time'] / d['exec_count'],
-        "AVG_RESOLVE_LOCK_TIME": d['sum_resolve_lock_time'] / d['exec_count'],
-        "AVG_LOCAL_LATCH_WAIT_TIME": d['sum_local_latch_time'] / d['exec_count'],
-        "MAX_LOCAL_LATCH_WAIT_TIME": d['max_local_latch_time'],
-        "AVG_WRITE_KEYS": d['sum_write_keys'] / d['exec_count'],
-        "AVG_WRITE_SIZE": d['sum_write_size'] / d['exec_count'],
-        "AVG_PREWRITE_REGIONS": d['sum_prewrite_region_num'] / d['exec_count'],
-        "MAX_PREWRITE_REGIONS": d['max_prewrite_region_num'],
-        "AVG_TXN_RETRY": d['sum_txn_retry'],
         "AVG_MEM": d['sum_mem'] / d['exec_count'],
-        "SUM_EXEC_RETRY": d['exec_retry_count'],
-        "SUM_EXEC_RETRY_TIME": d['exec_retry_time'],
         "AVG_DISK": d['sum_disk'] / d['exec_count'],
-        "AVG_KV_TIME": d['sum_kv_total'] / d['exec_count'],
-        "AVG_PD_TIME": d['sum_pd_total'] / d['exec_count'],
-        "AVG_BACKOFF_TOTAL_TIME": d['sum_backoff_total'] / d['exec_count'],
-        "AVG_WRITE_SQL_RESP_TIME": d['sum_write_sql_resp_total'] / d['exec_count'],
         "AVG_RESULT_ROWS": d['sum_result_rows'] / d['exec_count'],
         "AVG_AFFECTED_ROWS": d['sum_affected_rows'] / d['exec_count'],
         "INDEX_NAMES": stmt_index_names(d['index_names']),
 
         # TODO
-        "BACKOFF_TYPES": single_quote(''),
         "PLAN_HINT": single_quote(''),
         "PLAN": single_quote(''),
-        "MAX_REQUEST_UNIT_READ": 0,
-        "MAX_REQUEST_UNIT_WRITE": 0,
-        "MAX_QUEUED_RC_TIME": 0,
-        "AVG_REQUEST_UNIT_READ": 0,
-        "AVG_REQUEST_UNIT_WRITE": 0,
-        "AVG_QUEUED_RC_TIME": 0,
     }
     str_cols = ["STMT_TYPE", "SCHEMA_NAME", "DIGEST", "TABLE_NAMES", 
-                "MAX_COP_PROCESS_ADDRESS", "MAX_COP_WAIT_ADDRESS",
-                "PLAN_DIGEST", "CHARSET", "COLLATION", "FIRST_SEEN","LAST_SEEN"]
+                "PLAN_DIGEST", "FIRST_SEEN","LAST_SEEN"]
     for col in str_cols:
         cols[col] = single_quote(d[col.lower()])
 
-    num_cols = ["EXEC_COUNT", "SUM_ERRORS", "SUM_WARNINGS",
-                "SUM_LATENCY", "MAX_LATENCY", "MIN_LATENCY",
+    num_cols = ["EXEC_COUNT", "SUM_LATENCY", "MAX_LATENCY", "MIN_LATENCY",
                 "MAX_PARSE_LATENCY", "MAX_COMPILE_LATENCY", 
-                "MAX_COP_PROCESS_TIME", "MAX_COP_WAIT_TIME", 
-                "MAX_PROCESS_TIME", "MAX_WAIT_TIME",
-                "MAX_BACKOFF_TIME", "MAX_TOTAL_KEYS", 
-                "MAX_PROCESSED_KEYS", "MAX_ROCKSDB_DELETE_SKIPPED_COUNT",
-                "MAX_ROCKSDB_KEY_SKIPPED_COUNT",
-                "MAX_ROCKSDB_BLOCK_CACHE_HIT_COUNT",
-                "MAX_ROCKSDB_BLOCK_READ_COUNT", "MAX_ROCKSDB_BLOCK_READ_BYTE",
-                "MAX_PREWRITE_TIME","MAX_COMMIT_TIME",
-                "MAX_GET_COMMIT_TS_TIME","MAX_COMMIT_BACKOFF_TIME", "MAX_RESOLVE_LOCK_TIME",
-                "MAX_WRITE_KEYS","MAX_WRITE_SIZE", "MAX_TXN_RETRY",
-                "SUM_BACKOFF_TIMES","MAX_MEM","MAX_DISK","MAX_RESULT_ROWS",
-                "MIN_RESULT_ROWS","PREPARED", "PLAN_IN_CACHE","PLAN_CACHE_HITS","PLAN_IN_BINDING",
+                "MAX_MEM","MAX_DISK","PREPARED", "PLAN_IN_CACHE",
+                "PLAN_CACHE_HITS","PLAN_IN_BINDING",
                 ]
     for col in num_cols:
         cols[col] = d[col.lower()]
@@ -220,11 +67,20 @@ def s3stmt2sql(one_stmt_json):
     sql = "insert into statements_summary (%s) values (%s);" % (", ".join(sql_cols), ", ".join(sql_vals))
     return sql
 
-f = "./s3stmtlog.log"
-print(table_schema)
-with open(f, 'r') as file:
-    for line in file:
-        line = line.strip()
-        if line == "":
-            continue
-        print(s3stmt2sql(line))
+def s3stmtfile2sql(file):
+    base_name, _ = os.path.splitext(file)
+    output_file = base_name + ".sql"
+    
+    with open(output_file, 'w') as output:
+        output.write(statement_summary_schema)
+        output.write("\n")
+        with open(file, 'r') as input:
+            for line in input:
+                line = line.strip()
+                if line == "":
+                    continue
+                output.write(s3stmt2sql(line))
+                output.write("\n")
+
+s3stmtfile2sql("./testdata/s3stmtlog_10.log")
+s3stmtfile2sql("./testdata/s3stmtlog_100.log")
